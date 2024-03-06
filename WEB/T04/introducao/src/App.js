0453b3671390ec83b17509d43eed4f2ffc591db0
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-03-06 10:28:37 -0300

    atividade 00

--- WEB/T04/introducao/src/App.js
+++ WEB/T04/introducao/src/App.js
@@ -1,14 +1,47 @@
 //import HelloWorld from "./components/01HelloWorld/HelloWorld";
 //import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
-import Loja from "./components/03Props/Loja";
+//import Loja from "./components/03Props/Loja";
+//import {MeusDados as MD, MeusDadosV1 as MDV1, MeusDadosV2, MeusDadosV3} from "./components/00Atividades/atividade00/01MeusDados";
+//import MeusDadosProps from "./components/00Atividades/atividade00/02MeusDados";
+//import Temperatura from "./components/00Atividades/atividade00/03Temperatiura";
+//import SistemaSolar from "./components/04Children/SistemaSolar";
+//import Planeta from "./components/04Children/Planeta";
+import Galaxia from "./components/04Children/Galaxia";
 
 function App() {
   return (
     <div>
+      <h1>Desenvolvimento de Software para a WEB!</h1>
+      <Galaxia nome="Via Láctea" />
+    </div>
+  )
+}
+
+//questão do children
+/*function App() {
+  return (
+    <div>
       <h1> Desenvolvimento de Software para a WEB!</h1>
-      <Loja />
+      <hr />
+      <SistemaSolar>
+        <Planeta nome="Mercúrio" tipo="Rochoso" ordem="1" />
+        <hr />
+        <Planeta nome="Vênus" tipo="Rochoso" ordem="2" />
+        <hr />
+        <Planeta nome="Terra" tipo="Rochoso" ordem="3" />
+        <hr />
+        <Planeta nome="Marte" tipo="Rochoso" ordem="4" />
+        <hr />
+        <Planeta nome="Júpiter" tipo="Gasoso" ordem="5" />
+        <hr />
+        <Planeta nome="Saturno" tipo="Gasoso" ordem="6" />
+        <hr />
+        <Planeta nome="Urano" tipo="Gasoso" ordem="7" />
+        <hr />
+        <Planeta nome="Netuno" tipo="Gasoso" ordem="8" />
+      </SistemaSolar>
     </div>
   );
-}
+}*/
 
 export default App;
---
+++ WEB/T04/introducao/src/components/00Atividades/atividade00/02MeusDados.js
@@ -0,0 +1,13 @@
+
+const MeusDadosProps = ({nome, curso, universidade}, ira) => {
+    //let {nome, curso, universidade} = props
+    return (
+        <div>
+            <h2>Nome: {nome}</h2>
+            <h2>Curso: {curso}</h2>
+            <h2>Universidade: {universidade}</h2>
+        </div>
+    )
+}
+
+export default MeusDadosProps
