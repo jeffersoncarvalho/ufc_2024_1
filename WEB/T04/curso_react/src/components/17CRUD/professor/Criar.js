92e532adc3f1460e0d9290e498df1cb20ce6ce30
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-07-24 09:57:12 -0300

    crud listar bootstrap submit

--- WEB/T04/curso_react/src/components/17CRUD/professor/Criar.js
+++ WEB/T04/curso_react/src/components/17CRUD/professor/Criar.js
@@ -1,6 +1,71 @@
+import "../css/crud.css"
+
+import { useState } from "react"
+
 const Criar = () => {
+    
+    const [nome, setNome] = useState("")
+    const [curso, setCurso] = useState("")
+    const [titulacao, setTitulacao] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert("Nome: " + nome + "\n Curso: " + curso)
+    }
+    
     return (
-        <h1>Criar Professor</h1>
+        <div>
+            <h1>Criar Professor</h1>
+            <form className="form-content" onSubmit={handleSubmit}>
+
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="inputNome">Nome</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="nome" 
+                        id="inputNome"
+                        onChange={(event)=>setNome(event.target.value)}
+                    />
+                </div>
+                
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="inputCurso">Curso</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="curso"
+                        id="inputCurso"
+                        onChange={(event)=>setCurso(event.target.value)} 
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="selectTitulacao">Titulação</label> 
+                    <select
+                        className="form-select"
+                        value={titulacao}
+                        id="selectTitulacao"
+                    >
+                        <option value="GRADUACAO">GRADUAÇÃO</option>
+                        <option value="MESTRADO">MESTRADO</option>
+                        <option value="DOUTORADO">DOUTORADO</option>
+                    </select>
+                </div>
+                
+                <div className="div-button-submit">
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        style={{marginLeft:0}}
+                    >
+                        Submeter
+                    </button>
+                </div>
+
+            </form>
+        </div>
+        
     )
 }
 
