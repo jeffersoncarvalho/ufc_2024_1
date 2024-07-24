c8bba48d23fd067d9349a46b4f442bc01658ff21
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-07-24 08:25:00 -0300

    crud navbar

---
+++ WEB/T02/curso_react/src/components/15CRUD/data/db_aluno.js
@@ -0,0 +1,6 @@
+const alunos = [
+    { id:0, nome: "Zé", curso:"SI", cidade:"Limoeiro" },
+    { id:1, nome: "Zé", curso:"SI", cidade:"Limoeiro" },
+    { id:2, nome: "Zé", curso:"SI", cidade:"Limoeiro" },
+    { id:3, nome: "Zé", curso:"SI", cidade:"Limoeiro" },
+]
--- WEB/T04/curso_react/src/components/17CRUD/Home.js
+++ WEB/T04/curso_react/src/components/17CRUD/Home.js
@@ -1,14 +1,71 @@
-import { Outlet, Link } from "react-router-dom"
-const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/professor/listar"> LISTAR</Link>
-            <Link to="/professor/editar"> EDITAR</Link>
-            <Link to="/professor/criar"> CRIAR</Link>
-            <Outlet />
-        </div>   
-    )
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
-export default Home
+import { Link, Outlet } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      {/*INÍCIO DA BARRA DE NAVEGAÇÃO*/}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          
+          <a className="navbar-brand" href="www.bootstrap.com">
+            Navbar
+          </a>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="http://www.bootstrap.com">
+                  Home
+                </a>
+              </li>
+              {/*INÍCIO DO MENU DROPDOWN*/}
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Professor
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to="professor/listar">
+                        Listar Professor
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="professor/criar">
+                        Criar Professor
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              {/*FIM DO MENU DROPDOWN*/}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/*FIM DA BARRA DE NAVEGAÇÃO*/}
+      
+      {/*INÍCIO DO CONTEÚDO DO SITE*/}
+      <Outlet />
+      {/*FIM DO CONTEÚDO DO SITE*/}
+   
+    </div>
+  );
+};
+
+export default Home;
+
+/* 
+<h1>Home</h1>
+<Link to="/professor/listar"> LISTAR</Link>
+<Link to="/professor/editar"> EDITAR</Link>
+<Link to="/professor/criar"> CRIAR</Link>
+<Outlet />
+*/
