0e086252c1c718294c4a52c71d1d7e98a858bb50
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-07-24 09:07:08 -0300

    crud navbar

--- WEB/T04/curso_react/src/components/17CRUD/professor/Listar.js
+++ WEB/T04/curso_react/src/components/17CRUD/professor/Listar.js
@@ -1,20 +1,42 @@
-import professores from "../data/db_professor"
+import professores from "../data/db_professor";
 
 const Listar = () => {
-
-    const renderizarProfessores = () => {
-        const vetorResultado = professores.map(professor => JSON.stringify(professor)) 
-        return vetorResultado
-    }
-
-    return (
-        <>
-            <h1>Listar Professor</h1>
-            <div>
-                {renderizarProfessores()}
-            </div>
-        </>
+  const renderizarProfessores = () => {
+    const vetorResultado = professores.map(
+        (professor) => {
+            return (
+                <tr>
+                    <th scope="row">{professor.id}</th>
+                    <td>{professor.nome}</td>
+                    <td>{professor.curso}</td>
+                    <td>{professor.titulacao}</td>
+                </tr>
+            )
+        }
     )
-}
+    return vetorResultado;
+  };
 
-export default Listar
+  return (
+    <>
+      <h1>Listar Professor</h1>
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Curso</th>
+              <th scope="col">Titulação</th>
+            </tr>
+          </thead>
+          <tbody>
+            {renderizarProfessores()}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Listar;
