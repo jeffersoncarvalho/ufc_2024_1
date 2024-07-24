f744147a9ab77f997588077151fab5645c9e507c
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-07-24 09:22:06 -0300

    crud listar bootstrap button

--- WEB/T04/curso_react/src/components/17CRUD/professor/Listar.js
+++ WEB/T04/curso_react/src/components/17CRUD/professor/Listar.js
@@ -11,6 +11,12 @@ const Listar = () => {
                     <td>{professor.nome}</td>
                     <td>{professor.curso}</td>
                     <td>{professor.titulacao}</td>
+                    <td>
+                        <div>
+                            <button type="button" className="btn btn-secondary">Editar</button>
+                            <button type="button" className="btn btn-danger">Apagar</button>
+                        </div>
+                    </td>
                 </tr>
             )
         }
@@ -29,6 +35,7 @@ const Listar = () => {
               <th scope="col">Nome</th>
               <th scope="col">Curso</th>
               <th scope="col">Titulação</th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
