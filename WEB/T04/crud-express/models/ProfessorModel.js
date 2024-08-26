39a87541e5a26b997ec416c30bf29f42ee34acb4
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-08-26 10:27:43 -0300

    express

---
+++ WEB/T04/crud-express/models/ProfessorModel.js
@@ -0,0 +1,10 @@
+class ProfessorModel {
+    
+    constructor() {
+        this.id = id
+        this.nome = nome
+        this.curso = curso
+        this.titulacao = titulacao
+        this.universidade = universidade
+    }
+}
