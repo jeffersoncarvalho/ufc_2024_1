f832fc52703b054ac71ba18de628512fd06c6864
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2024-09-04 08:57:45 -0300

    mongoose

--- WEB/T04/crud-express/app.js
+++ WEB/T04/crud-express/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//MONGO
+require("./db/mongo")
 
 //importando os arquivos de rotas
 var indexRouter = require('./routes/index');
